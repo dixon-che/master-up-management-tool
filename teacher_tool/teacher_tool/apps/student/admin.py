feb5c52238fea24b44ef07d70f87847906d9d74f
Aleksey Radchenko <dixon.che@gmail.com>
2013-02-26 20:23:34 +0200

    Added model Group

--- teacher_tool/teacher_tool/apps/student/admin.py
+++ teacher_tool/teacher_tool/apps/student/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from teacher_tool.apps.student.models import Student
+from teacher_tool.apps.student.models import Student, Group
+
 
 admin.site.register(Student)
+admin.site.register(Group)
--- teacher_tool/teacher_tool/apps/student/models.py
+++ teacher_tool/teacher_tool/apps/student/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 
+class Group(models.Model):
+
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+
 class Student(models.Model):
 
+    group = models.ForeignKey(Group)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
