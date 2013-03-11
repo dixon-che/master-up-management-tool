2b3ac0e2f91f34999d949c00d761a73ef9bcec38
Aleksey Radchenko <dixon.che@gmail.com>
2013-03-11 18:17:56 +0200

    Editing student item

--- teacher_tool/teacher_tool/apps/student/views.py
+++ teacher_tool/teacher_tool/apps/student/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
+from django.core.urlresolvers import reverse
+from django.forms import ModelForm
+
 
 from teacher_tool.apps.student.models import Student, Group
 
@@ -28,3 +31,53 @@ def student_item_view(request, id):
     student = get_object_or_404(Student, id=id)
     return render(request, "student_item.html",
                   {'student': student})
+
+
+class StudentForm(ModelForm):
+
+    class Meta:
+        model = Student
+
+
+def student_item_edit(request, id=None):
+    if id is not None:
+        student = get_object_or_404(Student, id=id)
+    else:
+        student = None
+
+    if request.GET:
+        form = StudentForm(request.GET, instance=student)
+        if form.is_valid():
+            student = form.save()
+            return redirect(reverse('student_item', args=[student.id]))
+    else:
+        form = StudentForm(instance=student)
+
+    return render(request, "student_edit_item.html",
+                  {'student': student, 'form': form})
+
+# def student_item_edit(request, id=None):
+#     if id is not None:
+#         print request.GET
+
+#         student = get_object_or_404(Student, id=id)
+#         if request.GET:
+#             if 'first_name' in request.GET:
+#                 student.first_name = request.GET['first_name']
+#             if 'last_name' in request.GET:
+#                 student.last_name = request.GET['last_name']
+#             student.save()
+
+#     else:
+#         group = Group.objects.all()[0]
+#         student = Student()
+#         student.group = group
+#         student.first_name = "test"
+#         student.last_name = "testovich2"
+#         student.save()
+
+#         return redirect(reverse('student_item', args=[student.id]))
+
+#     return render(request, "student_edit_item.html",
+#                   {'student': student})
+
--- teacher_tool/teacher_tool/urls.py
+++ teacher_tool/teacher_tool/urls.py
@@ -8,6 +8,10 @@ urlpatterns = patterns('',
     url(r'^my_courses/$', 'teacher_tool.views.my_courses_view', name='my_courses_list'),
     url(r'^my_courses/course_item/$', 'teacher_tool.views.course_item_view', name='course_item'),
     url(r'^student/(?P<id>\d+)/$', 'teacher_tool.apps.student.views.student_item_view', name='student_item'),
+    url(r'^student/create/$', 'teacher_tool.apps.student.views.student_item_edit', name='student_create'),
+    url(r'^student/(?P<id>\d+)/edit/$', 'teacher_tool.apps.student.views.student_item_edit', name='student_edit'),
+
+
     url(r'^groups/', include('teacher_tool.apps.student.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
